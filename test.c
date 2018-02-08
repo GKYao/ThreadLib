7775cc1e0540f6ff5555230948498a6df806a759
Benjamin Samuel Weiss <31832941+Biter29@users.noreply.github.com>
2018-02-08 13:59:55 -0500

    Add files via upload

--- test.c
+++ test.c
@@ -3,23 +3,20 @@
 #include <stdio.h>
 #include <sys/time.h>
 #define stac 40000
-ucontext_t thr, orig,main_context;
+ucontext_t thr, orig,main_context,op_context;
 queue run_q;
-tcb *current;
+tcb *current=NULL;
+node* killer=NULL; //where threads go to be reaped
 //signal handler
 int count = 0;
 int id=0;
-void sighandler(int sig)
-{
-     printf("signal occurred %d times\n", ++count);
-}
+int SYS=0;
+struct itimerval it;
+struct sigaction act, oact;
 
 
-//start Itimer
-void start_itime(){
+void start_itime(){ //later put up a way to tell how much time using multileveled queue
 	//setup Itimer
-    struct itimerval it;
-    struct sigaction act, oact;
     act.sa_handler = sighandler;
     sigemptyset(&act.sa_mask);
     act.sa_flags = 0;
@@ -40,21 +37,83 @@ void stop_itime(){
 	
 }
 
+void sighandler(int sig)
+{	//must be fixed this is for changing priority
+	if(SYS=0){
+		tcb *swap_tcb = (tcb *) malloc(sizeof(tcb));
+		swap_tcb=current;
+		current=dequeue();
+		if(current==NULL){
+			current==swap_tcb;
+		}else{
+			enqueue(swap_tcb);
+		}	
+		start_itime();
+		setcontext(&current->uc);
+	}else{
+		return;
+	}    
+}
+
+
+//start Itimer
+void my_pthread_exit(void *value_ptr) {
+SYS=1;
+tcb *nswap_tcb = (tcb *) malloc(sizeof(tcb));// might want to include kill
+nswap_tcb=dequeue(); //when kill list is made to put for reapable threads is created, then edit code
+			//do it here
+current=nswap_tcb;
+start_itime();
+setcontext(&current->uc);
+};
+
+
 
 /* create a new thread */
 int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*function)(void*), void * arg) {
 //start Itimer
    			//replace with dequeue regarding getting thread, orig is not meant to be permanent.
 //get context
+	SYS=1;
+	if(current==NULL){
+
+	if(getcontext(&main_context)!=0){
+		printf("error\n");
+		exit(EXIT_FAILURE);
+	}
+		run_q.head = run_q.back = NULL;
+		run_q.size = 0;
+	//get main context
+		tcb *main_tcb = (tcb *) malloc(sizeof(tcb));
+    		main_tcb->id=id;//(IMPORTANT!!! main ID??????)
+		id=id+1;
+		if(getcontext(&main_tcb->uc)!=0){
+		printf("error\n");
+		exit(EXIT_FAILURE);
+		}
+	//set main as current thread context
+	    current = main_tcb;
+	}
+
 	if(getcontext(&thr)!=0){
 	printf("error\n");
 	exit(EXIT_FAILURE);
 	}
-	if(getcontext(&orig)!=0){
+	if(getcontext(&op_context)!=0){
 	printf("error\n");
 	exit(EXIT_FAILURE);
+	}	
+	op_context.uc_link=0;	//later change instead of 0 some other type of link to exit
+	op_context.uc_stack.ss_sp=(int*)malloc(stac);
+	op_context.uc_stack.ss_size=stac;
+	op_context.uc_stack.ss_flags=0;
+	if(arg==NULL){
+	makecontext(&op_context,(void*)(*my_pthread_exit),0);
 	}
-	thr.uc_link=0;
+
+
+
+	thr.uc_link=&op_context;	// some other type of link to exit so no problem is encountered
 	thr.uc_stack.ss_sp=(int*)malloc(stac);
 	thr.uc_stack.ss_size=stac;
 	thr.uc_stack.ss_flags=0;	
@@ -63,15 +122,39 @@ int my_pthread_create(my_pthread_t * thread, pthread_attr_t * attr, void *(*func
 	}else{
 	makecontext(&thr,(void*)(*function),1,arg);
 	}
+	tcb *new_tcb = (tcb *) malloc(sizeof(tcb));
+	new_tcb->id=id;
+	*thread=id;
+	id=id+1;		//all thar is occuring is a context switch with our new thread.
+	new_tcb->uc=thr;
+	tcb *temp_tcb = (tcb *) malloc(sizeof(tcb));
+	temp_tcb=current;
+	enqueue(current);
+	current=new_tcb; //now it is that our new tcb is going to run
 	start_itime();
-    swapcontext(&orig,&thr);	
+	SYS=0;
+    swapcontext(&temp_tcb->uc,&current->uc);	
 	return 0;
 };
 
- void myplay(){
-    printf("bob\n");
+ 
+void myfo(){
+    printf("billy\n");
+my_pthread_t * th=malloc(sizeof(my_pthread_t));
+//my_pthread_create(th,NULL, (void *)(*myplay),NULL);
 	my_pthread_yield();
-    setcontext(&current->uc);
+	
+//    setcontext(&current->uc);
+
+}
+
+void myplay(){
+    printf("bob\n");
+my_pthread_t * th=malloc(sizeof(my_pthread_t));
+my_pthread_create(th,NULL, (void *)(*myfo),NULL);
+	//my_pthread_yield();
+	printf("MOMMY\n");
+//    setcontext(&current->uc);
 
 }
 
@@ -102,7 +185,7 @@ int enqueue (tcb *thread) {
 
 
 //Initial thead queue by managing main context, this must be eliminated effective immediately as we cannot have an init later on.
-void thread_init(){
+/*void thread_init(){
 	//initialized queue
 	run_q.head = run_q.back = NULL;
 	run_q.size = 0;
@@ -117,16 +200,21 @@ void thread_init(){
 	//set main as current thread context
     current = main_tcb;
 	return;
-}
+}*/
 
 int my_pthread_yield() {
-	stop_itime();
-	printf("HAHAHA");
+	SYS=1;
+	tcb *tem_tcb = (tcb *) malloc(sizeof(tcb));
+	tem_tcb=dequeue();
+	//worry about null case next
+	if(tem_tcb!=NULL){
+	// dont do anything otherwise for now later change priority queue if encountering a yield and no other threads, rerunning not allowed
 	enqueue(current);
-	enqueue(current);
-	dequeue();
+	current=tem_tcb;
+	}
 	start_itime();
-	setcontext(&orig);
+	SYS=0;
+	setcontext(&current->uc);
 	return 0;
 };
 
@@ -134,11 +222,14 @@ int my_pthread_yield() {
 
 
 int main(){
-    thread_init();
-    printf("%d\n",current->id );
+   // thread_init();
+    //printf("%d\n",current->id );
 //Createthread
     my_pthread_t * thread=malloc(sizeof(my_pthread_t));
+	 
     my_pthread_create(thread,NULL, (void *)(*myplay),NULL);
+	my_pthread_yield();
+    
     printf("tim tam\n");
 
 
